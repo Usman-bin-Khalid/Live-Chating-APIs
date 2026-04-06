863db4273bb4cdfe4465f6dbd04ce75a99c3461c
Usman <usmanbinkhalidpk@gmail.com>
2026-04-06 09:45:18 +0500

    Auth Controller Completed

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = await User.create({ username, email, password });
+        res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.json({ token, user: { id: user._id, username: user.username } });
+    } else {
+        res.status(401).json({ message: "Invalid credentials" });
+    }
+};
+
+exports.updateProfile = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.user.id, req.body, { new: true });
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
